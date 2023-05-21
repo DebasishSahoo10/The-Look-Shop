79ae500811ad571f40988b8ee8d6c9bb318ea1bb
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-21 21:45:19 +0530

    app : removed un-required useReducer action cases

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -7,13 +7,15 @@ export const DataContext = createContext()
 const handleState = (state, action) => {
     switch (action.type) {
         case "SET_PRODUCTS" : return {...state, products:action.payload}
-        case "ADD_TO_CART" : return {...state, cart:[...state.cart, action.payload]}
-        case "ADD_TO_WISHLIST" : return {...state, wishlist:[...state.wishlist, action.payload]}
-        case "REMOVE_FROM_CART" : return {...state, cart:[...state.cart.filter(item => item!==action.payload)]}
-        case "REMOVE_FROM_WISHLIST" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)]}
-        case "MOVE_TO_CART" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)], cart : [...state.cart, action.payload]}
-        case "INCREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id) {return {...item, quantity: item.quantity+1}} else {return item}})]}
-        case "DECREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id && item.quantity>1) {return {...item, quantity: item.quantity-1}} else {return item}})]}
+        case "SET_CART" : return {...state, cart : action.payload}
+        case "SET_WISHLIST" : return {...state, wishlist : action.payload}
+        // case "ADD_TO_CART" : return {...state, cart:[...state.cart, action.payload]}
+        // case "ADD_TO_WISHLIST" : return {...state, wishlist:[...state.wishlist, action.payload]}
+        // case "REMOVE_FROM_CART" : return {...state, cart:[...state.cart.filter(item => item!==action.payload)]}
+        // case "REMOVE_FROM_WISHLIST" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)]}
+        // case "MOVE_TO_CART" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)], cart : [...state.cart, action.payload]}
+        // case "INCREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id) {return {...item, quantity: item.quantity+1}} else {return item}})]}
+        // case "DECREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id && item.quantity>1) {return {...item, quantity: item.quantity-1}} else {return item}})]}
         default : return state
     }
 }
