1f37105ec9d92433ea099b0c4a1d3ee989d52bee
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-22 13:16:25 +0530

    app : refactored component to include button components

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 import "./ProductList.css";
 import { FilterContext } from "../../contexts/FilterContext";
-import { AuthContext } from "../../contexts/AuthContext";
 import { DataContext } from "../../contexts/DataContext";
-import { cartHandler, wishlistHandler } from "../../utils/clickHandlers";
+import { AddToCart } from "./AddToCart";
+import { AddToWishlist } from "./AddToWishlist";
 
 export const ProductList = () => {
-  const { state, dispatch } = useContext(DataContext);
+  const { state } = useContext(DataContext);
   const { filters } = useContext(FilterContext);
-  const { authToken, isLoggedin } = useContext(AuthContext);
+
   let prodDB = state.products
     .filter(
       (item) =>
@@ -19,71 +19,29 @@ export const ProductList = () => {
           filters.category.includes(item.category)) &&
         item.rating >= filters.rating
     )
-    .sort((a, b) => {
-      if (filters.priceSort === "LTH") {
-        return a.price - b.price;
-      } else if (filters.priceSort === "HTL") {
-        return b.price - a.price;
-      } else {
-        // eslint-disable-next-line array-callback-return
-        return;
-      }
-    });
+    .sort((a, b) =>
+      filters.priceSort === "LTH"
+        ? a.price - b.price
+        : filters.priceSort === "HTL"
+        ? b.price - a.price
+        : 0
+    );
 
   return (
     <>
       {prodDB.map((item) => {
         return (
           <li key={item.id}>
-            <img src={item.img} alt="" width={"150px"} />
-            <div className="product-card-details">
+            <img src={item.img} alt={`look for ${item.name}`} width={"150px"} />
+            <div className="product-list_card-details">
               <NavLink to={`/products/${item.id}`}>
                 <p>{item.name}</p>
               </NavLink>
               <p>{item.price} $</p>
               <p>{item.rating} ⭐</p>
               <div style={{ display: "flex", flexWrap: "wrap", gap: "5px" }}>
-                {isLoggedin ? (
-                  <>
-                    {state.cart.some((prod) => prod.id === item.id) ? (
-                      <NavLink to="/cart">
-                        <button>Go to Cart</button>
-                      </NavLink>
-                    ) : (
-                      <button
-                        onClick={() => cartHandler(item, authToken, dispatch)}
-                      >
-                        Add to Cart
-                      </button>
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <NavLink to="/login">
-                      <button>Add to Cart</button>
-                    </NavLink>
-                  </>
-                )}
-
-                {isLoggedin ? (
-                  <>
-                    {state.wishlist.some((prod) => prod.id === item.id) ? (
-                  <button>Added to Wishlist</button>
-                ) : (
-                  <button
-                    onClick={() => wishlistHandler(item, authToken, dispatch)}
-                  >
-                    Add to Wishlist
-                  </button>
-                )}
-                  </>
-                ) : (
-                  <>
-                    <NavLink to="/login">
-                      <button className="wishlist-btn">Add to wishlist</button>
-                    </NavLink>
-                  </>
-                )}
+                <AddToCart item={item}/>
+                <AddToWishlist item={item}/>
               </div>
             </div>
           </li>
