1cf4002f71a2eda2451cf526ed1a0810863ab1d9
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-22 11:44:51 +0530

    app : simplified the component
    using generalized sub-component

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,71 +1,11 @@
-import { useContext } from "react";
-import { DataContext } from "../../contexts/DataContext";
-import {FilterContext} from "../../contexts/FilterContext"
-import { useNavigate } from "react-router-dom";
 import './Categories.css'
+import { CategorySection } from "./CategorySection";
 
 export const Categories = () => {
-  const { state } = useContext(DataContext);
-  const {dispatchFilters} = useContext(FilterContext)
-  const navigate = useNavigate()
-  const handleCategoryNavigate = (gender) => {
-    dispatchFilters({type : "SET_GENDER", payload : gender})
-    navigate('/products')
-  }
   return (
     <div className="categories">
-      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-        <div style={{ display: "flex", gap: "20px" }}>
-          <p>MEN CHARACTERS</p>
-          <a onClick={()=>handleCategoryNavigate("men")}>
-            <p style={{textDecoration : "underline"}}>SHOW ALL</p>
-          </a>
-        </div>
-        <div className="card-container">
-          {state.products
-            .filter((item) => item.gender === "men")
-            .map((item, index) => {
-              if (index < 4) {
-                return (
-                  <li key={item.id} className="product-card">
-                    <img
-                      src={item.img}
-                      alt=""
-                    />
-                    <p>{item.name}</p>
-                  </li>
-                );
-              }
-            })}
-        </div>
-      </div>
-      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-        <div style={{ display: "flex", gap: "20px" }}>
-          <p>WOMEN CHARACTERS</p>
-          <a onClick={()=>handleCategoryNavigate("women")}>
-            <p style={{textDecoration : "underline"}}>SHOW ALL</p>
-          </a>
-        </div>
-        <div className="card-container">
-          {state.products
-            .filter((item) => item.gender === "women")
-            .map((item, index) => {
-              if (index < 4) {
-                return (
-                  <li key={item.id} className="product-card">
-                    <img
-                      src={item.img}
-                      alt=""
-                      height={"200px"}
-                      width={"150px"}
-                    />
-                    <p>{item.name}</p>
-                  </li>
-                );
-              }
-            })}
-        </div>
-      </div>
+      <CategorySection gender="men" />
+      <CategorySection gender="women" />
     </div>
   );
 };
