e4ded59f79179b359b0a75e91ba5d435806f300f
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-22 23:52:47 +0530

    app : refactored the component
    by further making it modular

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./Cart.css";
@@ -8,15 +8,15 @@ import { DataContext } from "../../contexts/DataContext";
 import {
   cartItemDecrement,
   cartItemIncrement,
-  cartItemRemoval,
-  wishlistHandler
+  cartItemRemoval
 } from "../../utils/clickHandlers";
 import { AuthContext } from "../../contexts/AuthContext";
+import { AddToWishlist } from "../../components/ProductList/AddToWishlist";
+import { PriceCheckout } from "./PriceCheckout";
 
 export const Cart = () => {
   const { state, dispatch } = useContext(DataContext);
   const { authToken } = useContext(AuthContext);
-  const [discount, setDiscount] = useState(0);
   return (
     <div>
       <Logo />
@@ -31,83 +31,31 @@ export const Cart = () => {
                 state.cart.map((item) => {
                   return (
                     <li key={item.id}>
-                      <img
-                        src={item.img}
-                        alt=""
-                        width={"150px"}
-                        height={"200px"}
-                      />
-                      <div className="product-card-details">
-                        <NavLink to={`/products/${item.id}`}>
-                          <p>{item.name}</p>
-                        </NavLink>
-
+                      <img src={item.img} alt={`look for ${item.name}`} width={"150px"} height={"200px"}/>
+                      <div className="product-list_card-details">
+                        <NavLink to={`/products/${item.id}`}><p>{item.name}</p></NavLink>
                         <p>{item.price} $</p>
                         <p>Quantity : {item.qty}</p>
-                        <div
-                          style={{
-                            display: "flex",
-                            flexWrap: "wrap",
-                            gap: "5px",
-                          }}
-                        >
-                          <div style={{ display: "flex", gap: "5px" }}>
-                            <button
-                              onClick={() =>
-                                cartItemIncrement(item, authToken, dispatch)
-                              }
-                            >
-                              +
-                            </button>
-                            <button
-                              onClick={() =>
-                                item.qty > 1 && cartItemDecrement(item, authToken, dispatch)
-                              }
-                            >
+                        <div className="cart-btn-container">
+                          <div>
+                            <button onClick={() => item.qty > 1 && cartItemDecrement(item, authToken, dispatch)}>
                               -
                             </button>
+                            <button onClick={() => cartItemIncrement(item, authToken, dispatch)}>
+                              +
+                            </button>
                           </div>
-                          <button
-                            onClick={() =>
-                              cartItemRemoval(item, authToken, dispatch)
-                            }
-                          >
+                          <button onClick={() => cartItemRemoval(item, authToken, dispatch)}>
                             Remove from Cart
                           </button>
-                          {state.wishlist.some(
-                            (prod) => prod.id === item.id
-                          ) ? (
-                            <button>Added to Wishlist</button>
-                          ) : (
-                            <button
-                              onClick={() =>wishlistHandler(item, authToken, dispatch)}
-                            >
-                              Add to Wishlist
-                            </button>
-                          )}
+                          <AddToWishlist item={item}/>
                         </div>
                       </div>
                     </li>
                   );
                 })}
             </div>
-            <div className="price-container">
-              {state.cart.length > 0 && (
-                <button onClick={() => setDiscount(55)}>Apply a Coupon</button>
-              )}
-              <p>Total items in Cart : {state.cart.length}</p>
-              <p>Total Discount : {discount}$</p>
-              <p>Delivery Charges : FREE</p>
-              <p>
-                Total Amount :{" "}
-                {state.cart.reduce(
-                  (acc, item) => (acc += item.price * item.qty),
-                  0
-                ) - discount}
-                $
-              </p>
-              <button>Checkout</button>
-            </div>
+            <PriceCheckout/>
           </>
         )}
       </div>
