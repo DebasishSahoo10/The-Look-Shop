588cad0a5bb5b3ef04d4a17f82b9afe3bb47f418
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-22 15:05:36 +0530

    style : designed anchor tags

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -5,8 +5,9 @@ export const Socials = () => {
     <div className="social-board">
       <p>Follow Us :</p>
       <div>
-        <p>the birdy app</p>
-        <p>that gradient one</p>
+        <a href="https://twitter.com/dddddddeeeeevvv" target="_blank" rel="noreferrer"><p className="twitter">the birdy app</p></a>
+        <a href="https://www.instagram.com/yoursimagifier/" target="_blank" rel="noreferrer"><p className="instagram">that gradient one</p></a>
+        <a href="https://github.com/DebasishSahoo10/The-Look-Shop" target="_blank" rel="noreferrer"><p className="github">octocat holds the code</p></a>
       </div>
     </div>
   );
