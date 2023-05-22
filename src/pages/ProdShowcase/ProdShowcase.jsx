94de433ff5fa7622c053f309644a2ca2f1a66fd4
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-23 00:18:23 +0530

    app : refactored the component

--- src/pages/ProdShowcase/ProdShowcase.jsx
+++ src/pages/ProdShowcase/ProdShowcase.jsx
@@ -1,12 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../../contexts/DataContext";
-import { useParams, NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Nav } from "../../components/Nav/Nav";
 import { Logo } from "../../components/Logo/Logo";
 import "./ProdShowcase.css";
+import '../Cart/Cart.css'
+import { AddToCart } from "../../components/ProductList/AddToCart";
+import { AddToWishlist } from "../../components/ProductList/AddToWishlist";
 
 export const ProdShowcase = () => {
-  const { state, dispatch } = useContext(DataContext);
+  const { state } = useContext(DataContext);
   const { prodID } = useParams();
   const [prod, setProd] = useState({});
   useEffect(() => {
@@ -19,32 +22,14 @@ export const ProdShowcase = () => {
       <div className="prod-card-holder">
         <div className="prod-card">
           <img src={prod.img} alt="" />
-          <div className="product-card-details">
+          <div className="product-list_card-details">
             <p>{prod.name}</p>
             <p>{prod.details}</p>
             <p>{prod.price} $</p>
             <p>{prod.rating} ⭐</p>
-            <div style={{ display: "flex", flexWrap: "wrap", gap: "5px" }}>
-              {state.cart.some((item) => item.id === prod.id) ? (
-                <NavLink to="/cart">
-                  <button>Go to Cart</button>
-                </NavLink>
-              ) : (
-                <button
-                  onClick={() =>
-                    dispatch({ type: "MOVE_TO_CART", payload: prod })
-                  }
-                >
-                  Move to Cart
-                </button>
-              )}
-              <button
-                onClick={() =>
-                  dispatch({ type: "REMOVE_FROM_WISHLIST", payload: prod })
-                }
-              >
-                Remove from Wishlist
-              </button>
+            <div className="cart-btn-container">
+              <AddToCart item={prod}/>
+              <AddToWishlist item={prod}/>
             </div>
           </div>
         </div>
