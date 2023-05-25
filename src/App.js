23e63893e6b0067e033ba255119220b75901f5c4
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-26 00:25:05 +0530

    feature : added lazy loading
    - to optimize the performance

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
+import { ToastContainer } from "react-toastify";
+import { Suspense, lazy } from "react";
+import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes } from "react-router";
-import Mockman from "mockman-js";
 
 import "./App.css";
-import "react-toastify/dist/ReactToastify.css";
-import { Landing } from "./pages/Landing";
-import { Products } from "./pages/Products";
-import { Cart } from "./pages/Cart/Cart";
-import { Wishlist } from "./pages/Wishlist/Wishlist";
-import { ProdShowcase } from "./pages/ProdShowcase/ProdShowcase";
-import { Login } from "./pages/Login/Login";
+import Landing from "./pages/Landing";
 import { RequiresAuth } from "./components/RequiresAuth";
-import { ToastContainer } from "react-toastify";
-import { Signup } from "./pages/Signup/Signup";
+import { LoadingFallback } from "./components/LoadingFallback";
+const Signup = lazy(() => import("./pages/Signup/Signup"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const ProdShowcase = lazy(() => import("./pages/ProdShowcase/ProdShowcase"));
+const Wishlist = lazy(() => import("./pages/Wishlist/Wishlist"));
+const Cart = lazy(() => import("./pages/Cart/Cart"));
+const Products = lazy(() => import("./pages/Products"));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/cart"
-          element={
-            <RequiresAuth>
-              <Cart />
-            </RequiresAuth>
-          }
-        />
-        <Route path="/products/" element={<Products />} />
-        <Route path="/products/:prodID" element={<ProdShowcase />} />
-        <Route
-          path="/wishlist"
-          element={
-            <RequiresAuth>
-              <Wishlist />
-            </RequiresAuth>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mockman" element={<Mockman />} />
-        <Route path="/signup" element = {<Signup/>}/>
-      </Routes>
-      <ToastContainer/>
+      <Suspense fallback={<LoadingFallback/>}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/cart"
+            element={
+              <RequiresAuth>
+                <Cart />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/products/" element={<Products />} />
+          <Route path="/products/:prodID" element={<ProdShowcase />} />
+          <Route
+            path="/wishlist"
+            element={
+              <RequiresAuth>
+                <Wishlist />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Suspense>
+      <ToastContainer />
     </>
   );
 }
