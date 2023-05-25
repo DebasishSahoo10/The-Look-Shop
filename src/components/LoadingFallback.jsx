2e8ada87471eece88c388b0f7e15c992b7c4a566
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-26 00:34:37 +0530

    style : added margin to fallback screens

--- src/components/LoadingFallback.jsx
+++ src/components/LoadingFallback.jsx
@@ -1,6 +1,6 @@
 export const LoadingFallback = () => {
     return (
-        <div style={{display:"flex", width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", fontFamily:"monospace"}}>
+        <div style={{display:"flex", width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", fontFamily:"monospace", margin:"10px"}}>
             <h1>Your Page is Loading. Wait a Moment...</h1>
         </div>
     )
