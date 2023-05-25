7d08b068a12c4f23bfe0b673608bc46a68b704f6
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-26 00:25:27 +0530

    feature : created fall back component

---
+++ src/components/LoadingFallback.jsx
@@ -0,0 +1,7 @@
+export const LoadingFallback = () => {
+    return (
+        <div style={{display:"flex", width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", fontFamily:"monospace"}}>
+            <h1>Your Page is Loading. Wait a Moment...</h1>
+        </div>
+    )
+}
