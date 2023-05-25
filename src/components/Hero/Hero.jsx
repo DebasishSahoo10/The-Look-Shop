d77073f837e8af12d5a02a7117cb4d1ee4b851be
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-05-26 00:44:05 +0530

    fix : added fetch priority to improve LCP

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,18 @@
-import {NavLink} from 'react-router-dom'
-import "./Hero.css"
+import { NavLink } from "react-router-dom";
+import "./Hero.css";
 
 export const Hero = () => {
   return (
     <div className="hero">
       <div>
         <p>The one-stop shop to buy a look for your metaverse character</p>
-        <NavLink to="/products"><button>Browse All</button></NavLink>
+        <NavLink to="/products">
+          <button>Browse All</button>
+        </NavLink>
       </div>
       <div>
         <img
+          fetchpriority="high"
           src="https://i.postimg.cc/x8GD3DNQ/Hero-Image.webp"
           alt="three models wearing three different looks"
         />
