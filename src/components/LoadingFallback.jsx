1cad9eb9d1cc087d5f94507e5c4129edd92e0150
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-06-01 01:03:30 +0530

    fix : fallback overflow viewport in mobile

--- src/components/LoadingFallback.jsx
+++ src/components/LoadingFallback.jsx
@@ -1,6 +1,6 @@
 export const LoadingFallback = () => {
     return (
-        <div style={{display:"flex", width:"100vw", height:"100vh", justifyContent:"center", alignItems:"center", fontFamily:"monospace", margin:"10px", textAlign:"center"}}>
+        <div style={{display:"flex", width:"95vw", height:"100vh", justifyContent:"center", alignItems:"center", fontFamily:"monospace", margin:"10px", textAlign:"center"}}>
             <h1>Your Page is Loading. Wait a Moment...</h1>
         </div>
     )
