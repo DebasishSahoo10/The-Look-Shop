66175199b49026487ee7339f525959ee8083e5ca
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-08-03 23:48:43 +0530

    feat : added debouncing in search bar

--- src/components/Filters/SearchBar.jsx
+++ src/components/Filters/SearchBar.jsx
@@ -1,12 +1,17 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { FilterContext } from "../../contexts/FilterContext"
 
 export const SearchBar = () => {
     const {dispatchFilters} = useContext(FilterContext)
+    const timerID = useRef(null)
+    const handleSearch = (searchParams) => {
+        clearTimeout(timerID)
+        timerID.current = setTimeout(()=>dispatchFilters({type : "ADD_SEARCH", payload : searchParams}), 500)
+    }
     return (
         <div className="filter_search-bar">
             <p>Search</p>
-            <input type="text" name="" id="" onChange={(e)=>dispatchFilters({type : "ADD_SEARCH", payload : e.target.value})}/>
+            <input type="text" name="" id="" onChange={(e)=>handleSearch(e.target.value)}/>
         </div>
     )
 }
