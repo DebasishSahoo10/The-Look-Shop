d5df2433a97032ee7584716c9b1b507301e9a05e
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-09-20 02:35:26 +0530

    Slices for Master Data

--- src/Redux/DataSlice.jsx
+++ src/Redux/DataSlice.jsx
@@ -1,6 +1,30 @@
 import { createAction, createSlice } from "@reduxjs/toolkit";
 
+const initialState = {products : [], cart : [], wishlist : []}
+
 export const DataSlice = createSlice({
     name : 'data',
-    initialState : null
+    initialState,
+    reducers : {
+        SET_PRODUCTS : (state, action) => {
+            console.log(action.payload)
+            state.products = action.payload
+        },
+        SET_CART : (state, action) => {
+            state.cart = action.payload
+        },
+        SET_WISHLIST : (state, action) => {
+            state.wishlist = action.payload
+        }
+    }
 })
+
+export const SET_PRODUCTS = createAction('data/SET_PRODUCTS', (products) => ({
+    payload : products
+}))
+export const SET_CART = createAction('data/SET_CART', (cart) => ({
+    payload : cart
+}))
+export const SET_WISHLIST = createAction('data/SET_WISHLIST', (wishlist) => ({
+    payload : wishlist
+}))
--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -9,13 +9,6 @@ const handleState = (state, action) => {
         case "SET_PRODUCTS" : return {...state, products:action.payload}
         case "SET_CART" : return {...state, cart : action.payload}
         case "SET_WISHLIST" : return {...state, wishlist : action.payload}
-        // case "ADD_TO_CART" : return {...state, cart:[...state.cart, action.payload]}
-        // case "ADD_TO_WISHLIST" : return {...state, wishlist:[...state.wishlist, action.payload]}
-        // case "REMOVE_FROM_CART" : return {...state, cart:[...state.cart.filter(item => item!==action.payload)]}
-        // case "REMOVE_FROM_WISHLIST" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)]}
-        // case "MOVE_TO_CART" : return {...state, wishlist : [...state.wishlist.filter(item => item !== action.payload)], cart : [...state.cart, action.payload]}
-        // case "INCREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id) {return {...item, quantity: item.quantity+1}} else {return item}})]}
-        // case "DECREASE_QUANTITY" : return {...state, cart:[...state.cart.map(item => {if (item.id === action.payload.id && item.quantity>1) {return {...item, quantity: item.quantity-1}} else {return item}})]}
         default : return state
     }
 }
@@ -23,19 +16,6 @@ const handleState = (state, action) => {
 export const DataProvider = ({children}) => {
     
     const [state, dispatch] = useReducer(handleState, {products : [], cart : [], wishlist : []})
-
-    useEffect(()=>{
-        (async()=>{
-            try {
-                const serverCall = await fetch("/api/products")
-                const products = await serverCall.json()
-                dispatch({type:"SET_PRODUCTS", payload: products.products})
-            } catch(err) {
-                console.log(err)
-            }
-        })()
-    },[])
-
     
 
     return (
