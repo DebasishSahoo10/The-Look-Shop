6f5b04fb4124c6060391599d511dd14c9f09c322
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-09-20 04:07:00 +0530

    Search Bar Fix

--- src/components/Filters/SearchBar.jsx
+++ src/components/Filters/SearchBar.jsx
@@ -1,19 +1,20 @@
-import { useRef } from "react"
-
+import { useRef, useState } from "react"
 import { useDispatch} from "react-redux"
 import { ADD_SEARCH } from "../../Redux/FilterSlice"
 
 export const SearchBar = () => {
+    const [searchParams, setSearchParams] = useState()
     const dispatch = useDispatch()
     const timerID = useRef(null)
     const handleSearch = (searchParams) => {
+        setSearchParams(searchParams)
         clearTimeout(timerID)
         timerID.current = setTimeout(()=>dispatch(ADD_SEARCH(searchParams)), 500)
     }
     return (
         <div className="filter_search-bar">
             <p>Search</p>
-            <input type="text" name="" id="" onChange={(e)=>handleSearch(e.target.value)}/>
+            <input type="text" name="" id="" onChange={(e)=>handleSearch(e.target.value)} value={searchParams}/>
         </div>
     )
 }
