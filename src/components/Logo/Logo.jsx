56462ceb42a1d50f53aa7fbd01e9a20929de2388
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-10-03 22:34:23 +0530

    Installed Dependencies

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import "./Logo.css"
-
-export const Logo = () => {
-  return (
-    <div className="logo">
-      <h1>THE LOOK SHOP</h1>
-    </div>
-  );
-};
--- src/components/RequiresAuth.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, useLocation } from "react-router"
-import { useSelector } from "react-redux"
-
-export const RequiresAuth = ({children}) => {
-    const location = useLocation()
-    const auth = useSelector(state => state.auth)
-    const isLoggedin = auth.isLoggedin
-    return isLoggedin ? (children) : (<Navigate to="/login" state={{from :location}}/>)
-}
